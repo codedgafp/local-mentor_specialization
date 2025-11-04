af84d244dfa323ede21186d0ee516b532b10e4bc
Agnes Lapeyronnie <agnes.lapeyronnie@cgi.com>
2025-11-04 06:53:49 +0000

    fix(block_sharing_cart): #MEN-896 Fix untranslated elements in sharingcart block

---
+++ data/lang/fr_local/block_sharing_cart.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['copy'] = 'Copier';
+$string['copy_quiz_warning'] = 'Attention ! - Vous êtes sur le point de copier un test dans le panier d\'activités. Cela peut prendre un certain temps si le cours comporte beaucoup de questions.';
+$string['sharing_cart:manual_run_task'] = 'Exécutez manuellement la tâche de sauvegarde/restauration.';   

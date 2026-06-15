ed28e19146cbef35f53a88147f4674f10bebef5a
Alban PLOQUIN <alban.ploquin@cgi.com>
2026-06-15 14:03:23 +0200

    fix(completion lang): #MEN-1342: update some text for Mentor

---
+++ data/lang/fr_local/block_completion_monitor.php
@@ -0,0 +1,7 @@
+<?php
+
+$string['pluginname'] = 'Suivi d\'achèvement des activités de la formation';
+$string['courseprogress_percentage'] = 'Formation complétée à {$a}%';
+$string['completion_monitor:addinstance'] = 'Autoriser l\'ajout du bloc Suivi d\'achèvement des activités de la formation';
+$string['activities_list_label'] = 'Activités de la formation';
+$string['region-top-block'] = 'Zone supérieure de la formation';

351a66b7486d665760438062a4e55e26a754879d
Alban PLOQUIN <alban.ploquin@cgi.com>
2026-07-07 14:03:19 +0200

    feat(completion monitor): #MEN-1387: set specific lang for course_progress_disabled key

--- data/lang/fr_local/block_completion_monitor.php
+++ data/lang/fr_local/block_completion_monitor.php
@@ -5,3 +5,4 @@ $string['courseprogress_percentage'] = 'Formation complétée à {$a}%';
 $string['completion_monitor:addinstance'] = 'Autoriser l\'ajout du bloc Suivi d\'achèvement des activités de la formation';
 $string['activities_list_label'] = 'Activités de la formation';
 $string['region-top-block'] = 'Zone supérieure de la formation';
+$string['course_progress_disabled'] = "Le suivi d'achèvement est désactivé au niveau des paramètres de la formation. Ce bloc ne sera donc pas visible par les utilisateurs.";
